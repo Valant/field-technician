6440961d360356150e8652def66cce49190c44a7
Mihail Shumilov <mschumilow@gmail.com>
2014-11-21 17:07:55 +0200

    move queries to controller

--- web/advanced/common/models/SVServiceTicket.php
+++ web/advanced/common/models/SVServiceTicket.php
@@ -3,6 +3,8 @@
     namespace common\models;
 
     use Yii;
+    use yii\data\ActiveDataProvider;
+    use yii\db\Query;
 
     /**
      * This is the model class for table "SV_Service_Ticket".
@@ -284,6 +286,68 @@
         public function getProblem()
         {
             return $this->hasMany( 'SVProblem', [ 'problem_id' => 'problem_id' ] );
+        }
+
+        public static function getList($service_tech_id){
+            $query = new Query;
+
+            return new ActiveDataProvider( [
+                'query' => $query->select( '
+                    SV_Service_Ticket.Service_Ticket_Id, SV_Problem.Description AS ProblemDescription,
+                    AR_Customer.Customer_Name, ar_customer_site.ge1_description as  City,
+                    SV_Service_Ticket.Ticket_Status
+                ' )->from( 'SV_Service_Ticket' )
+                                 ->innerJoin( 'SV_Service_Ticket_Dispatch',
+                                     'SV_Service_Ticket_Dispatch.Service_Ticket_Id = SV_Service_Ticket.Service_Ticket_Id' )
+                                 ->innerJoin( 'SV_Service_Tech',
+                                     'SV_Service_Tech.Service_Tech_Id = SV_Service_Ticket_Dispatch.Service_Tech_Id' )
+                                 ->innerJoin( 'SV_Problem', 'SV_Problem.Problem_Id = SV_Service_Ticket.Problem_Id' )
+                                 ->innerJoin( 'AR_Customer', 'AR_Customer.Customer_Id = SV_Service_Ticket.Customer_Id' )
+                                 ->innerJoin( 'AR_Customer_Site',
+                                     'AR_Customer_Site.Customer_Site_Id = SV_Service_Ticket.Customer_Site_Id' )
+                                 ->where( "SV_Service_Ticket.Ticket_Status = 'SC' AND SV_Service_Tech.Service_Tech_Id = :Service_Tech_Id",
+                                     [":Service_Tech_Id"=>$service_tech_id] )
+                                 ->orderBy( 'SV_Service_Ticket.Service_Ticket_Id', 'DESC' )->limit( 100 )
+            ] );
+
+        }
+
+        public static function getSingleInfo($ticket_id){
+            $query = new Query;
+
+
+            return new ActiveDataProvider( [
+                'query' => $query->select( '
+                SV_Service_Ticket.CustomerComments, SV_Service_Ticket.Service_Ticket_Id, SV_Service_Ticket.Ticket_Number,
+                SV_Service_Ticket.Customer_Id, SV_Service_Ticket.Requested_By, SV_Service_Ticket.Estimated_Length,
+                SV_Service_Ticket.Creation_Date, SV_Service_Ticket.Scheduled_For, SV_Service_Ticket.Category_Id,
+                SV_Service_Ticket.Route_Id, SV_Routes.Route_Code, SV_Routes.Description as  RouteDescription, SV_Problem.Problem_Code,
+                SV_Problem.Description as ProblemDescription, AR_Customer.Customer_Name, ar_customer.customer_number,
+                ar_customer_bill.address_1, ar_customer_bill.ge1_description, ar_customer_bill.ge2_short, ar_customer_bill.ge3_description,
+                ar_customer_site.business_name, ar_customer_site.address_1 as Customer_Site_Address, ar_customer_site.ge1_description as Customer_Site_Ge1_Description,
+                ar_customer_site.ge2_short as Customer_Site_Ge2_Short, ar_customer_site.ge3_description as Customer_Site_Ge3_Description,
+                ar_customer_system.alarm_account, sy_system.description as System_Description, sy_panel_type.description as System_Panel_Description,
+                ar_customer_site.phone_1, ar_customer_site.cross_street, ar_customer_system.system_comments, sv_service_ticket.ticket_status,
+                sv_service_ticket.entered_by, sv_service_ticket.requested_by_phone
+                ' )->from( 'SV_Service_Ticket' )
+                                 ->innerJoin( 'AR_Customer', 'AR_Customer.Customer_Id = SV_Service_Ticket.Customer_Id' )
+                                 ->innerJoin( 'AR_Customer_Bill',
+                                     'AR_Customer_Bill.Customer_Id = AR_Customer.Customer_Id' )
+                                 ->innerJoin( 'AR_Customer_Site',
+                                     'AR_Customer_Site.Customer_Site_Id = SV_Service_Ticket.Customer_Site_Id' )
+                                 ->innerJoin( 'AR_Customer_System',
+                                     'AR_Customer_System.Customer_Id = AR_Customer.Customer_Id' )
+                                 ->innerJoin( 'SY_System', 'AR_Customer_System.System_Id = SY_System.System_Id' )
+                                 ->innerJoin( 'SY_Panel_Type',
+                                     'SY_Panel_Type.panel_type_id = ar_customer_system.panel_type_id' )
+                                 ->innerJoin( 'SV_Service_Tech_Routes',
+                                     'SV_Service_Tech_Routes.Route_Id = SV_Service_Ticket.Route_Id' )
+                                 ->innerJoin( 'SV_Problem', 'SV_Service_Ticket.Problem_Id = SV_Problem.Problem_Id' )
+                                 ->innerJoin( 'SV_Routes', 'SV_Routes.Route_Id = SV_Service_Tech_Routes.Route_Id' )
+                                 ->where( "SV_Service_Ticket.Service_Ticket_Id = :Service_Ticket_Id",
+                                     [":Service_Ticket_Id"=>$ticket_id] )
+                                 ->limit( 1 )
+            ] );
         }
 
     }
--- web/advanced/frontend/controllers/TicketController.php
+++ web/advanced/frontend/controllers/TicketController.php
@@ -8,10 +8,10 @@
 
     namespace frontend\controllers;
 
+    use common\models\SVServiceTech;
+    use common\models\SVServiceTicket;
     use Yii;
     use yii\rest\ActiveController;
-    use yii\data\ActiveDataProvider;
-    use yii\db\Query;
 
 
     class TicketController extends ActiveController
@@ -20,77 +20,19 @@
 
         public function actionList()
         {
-            $whereArr = [ ];
             if (isset( $_REQUEST['Service_Tech_Id'] )) {
-                $whereArr[':Service_Tech_Id'] = (int) $_REQUEST['Service_Tech_Id'];
+                return SVServiceTicket::getList( (int) $_REQUEST['Service_Tech_Id'] );
+            } else {
+                return [ "status" => "error", "message" => "Service_Tech_Id params is required" ];
             }
-
-            $query = new Query;
-
-            return new ActiveDataProvider( [
-                'query' => $query->select( '
-                    SV_Service_Ticket.Service_Ticket_Id, SV_Problem.Description AS ProblemDescription,
-                    AR_Customer.Customer_Name, ar_customer_site.ge1_description as  City,
-                    SV_Service_Ticket.Ticket_Status
-                ' )->from( 'SV_Service_Ticket' )
-                                 ->innerJoin( 'SV_Service_Ticket_Dispatch',
-                                     'SV_Service_Ticket_Dispatch.Service_Ticket_Id = SV_Service_Ticket.Service_Ticket_Id' )
-                                 ->innerJoin( 'SV_Service_Tech',
-                                     'SV_Service_Tech.Service_Tech_Id = SV_Service_Ticket_Dispatch.Service_Tech_Id' )
-                                 ->innerJoin( 'SV_Problem', 'SV_Problem.Problem_Id = SV_Service_Ticket.Problem_Id' )
-                                 ->innerJoin( 'AR_Customer', 'AR_Customer.Customer_Id = SV_Service_Ticket.Customer_Id' )
-                                 ->innerJoin( 'AR_Customer_Site',
-                                     'AR_Customer_Site.Customer_Site_Id = SV_Service_Ticket.Customer_Site_Id' )
-                                 ->where( "SV_Service_Ticket.Ticket_Status = 'SC' AND SV_Service_Tech.Service_Tech_Id = :Service_Tech_Id",
-                                     $whereArr )
-                                 ->orderBy( 'SV_Service_Ticket.Service_Ticket_Id', 'DESC' )->limit( 100 )
-            ] );
         }
 
         public function actionFind()
         {
-
-            $whereArr = [ ];
-
             if (isset( $_REQUEST['id'] )) {
-                $whereArr[':Service_Ticket_Id'] = (int) $_REQUEST['id'];
+                return SVServiceTicket::getSingleInfo( $_REQUEST['id'] );
+            } else {
+                return [ "status" => "error", "message" => "ID params is required" ];
             }
-
-            $query = new Query;
-
-
-            return new ActiveDataProvider( [
-                'query' => $query->select( '
-                SV_Service_Ticket.CustomerComments, SV_Service_Ticket.Service_Ticket_Id, SV_Service_Ticket.Ticket_Number,
-                SV_Service_Ticket.Customer_Id, SV_Service_Ticket.Requested_By, SV_Service_Ticket.Estimated_Length,
-                SV_Service_Ticket.Creation_Date, SV_Service_Ticket.Scheduled_For, SV_Service_Ticket.Category_Id,
-                SV_Service_Ticket.Route_Id, SV_Routes.Route_Code, SV_Routes.Description as  RouteDescription, SV_Problem.Problem_Code,
-                SV_Problem.Description as ProblemDescription, AR_Customer.Customer_Name, ar_customer.customer_number,
-                ar_customer_bill.address_1, ar_customer_bill.ge1_description, ar_customer_bill.ge2_short, ar_customer_bill.ge3_description,
-                ar_customer_site.business_name, ar_customer_site.address_1 as Customer_Site_Address, ar_customer_site.ge1_description as Customer_Site_Ge1_Description,
-                ar_customer_site.ge2_short as Customer_Site_Ge2_Short, ar_customer_site.ge3_description as Customer_Site_Ge3_Description,
-                ar_customer_system.alarm_account, sy_system.description as System_Description, sy_panel_type.description as System_Panel_Description,
-                ar_customer_site.phone_1, ar_customer_site.cross_street, ar_customer_system.system_comments, sv_service_ticket.ticket_status,
-                sv_service_ticket.entered_by, sv_service_ticket.requested_by_phone
-                ' )->from( 'SV_Service_Ticket' )
-                                 ->innerJoin( 'AR_Customer', 'AR_Customer.Customer_Id = SV_Service_Ticket.Customer_Id' )
-                                 ->innerJoin( 'AR_Customer_Bill',
-                                     'AR_Customer_Bill.Customer_Id = AR_Customer.Customer_Id' )
-                                 ->innerJoin( 'AR_Customer_Site',
-                                     'AR_Customer_Site.Customer_Site_Id = SV_Service_Ticket.Customer_Site_Id' )
-                                 ->innerJoin( 'AR_Customer_System',
-                                     'AR_Customer_System.Customer_Id = AR_Customer.Customer_Id' )
-                                 ->innerJoin( 'SY_System', 'AR_Customer_System.System_Id = SY_System.System_Id' )
-                                 ->innerJoin( 'SY_Panel_Type',
-                                     'SY_Panel_Type.panel_type_id = ar_customer_system.panel_type_id' )
-                                 ->innerJoin( 'SV_Service_Tech_Routes',
-                                     'SV_Service_Tech_Routes.Route_Id = SV_Service_Ticket.Route_Id' )
-                                 ->innerJoin( 'SV_Problem', 'SV_Service_Ticket.Problem_Id = SV_Problem.Problem_Id' )
-                                 ->innerJoin( 'SV_Routes', 'SV_Routes.Route_Id = SV_Service_Tech_Routes.Route_Id' )
-                                 ->where( "SV_Service_Ticket.Service_Ticket_Id = :Service_Ticket_Id",
-                                     $whereArr )
-                                 ->limit( 1 )
-            ] );
-
         }
     }
