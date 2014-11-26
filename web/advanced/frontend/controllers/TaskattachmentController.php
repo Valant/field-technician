eb9a2c3c4f86445a374448ebcebb2b2f3a9d3ec1
krom1 <mihail.shumilov@valant.com.ua>
2014-11-26 05:19:28 -0500

    add new taskattachemtn

---
+++ web/advanced/frontend/controllers/TaskattachmentController.php
@@ -0,0 +1,46 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: godson
+     * Date: 06.10.14
+     * Time: 13:44
+     */
+
+    namespace frontend\controllers;
+
+    use Yii;
+    use common\models\TaskAttachment;
+    use yii\rest\ActiveController;
+
+
+    class TaskattachmentController extends ActiveController
+    {
+        public $modelClass = 'common\models\TaskAttachment';
+
+        public function actionUpload()
+        {
+            foreach ($_FILES as $file) {
+                $fileName = mt_rand( 0, PHP_INT_MAX ) . "_" . $file['name'];
+                $fileUrl  = "/web/uploads/" . $_REQUEST['task_id'] . "/";
+                $filePath = Yii::getAlias( Yii::$app->params['filePath'] ) . $fileUrl . $fileName;
+                if ( ! is_dir( Yii::getAlias( Yii::$app->params['filePath'] ) . $fileUrl )) {
+                    mkdir( Yii::getAlias( Yii::$app->params['filePath'] ) . $fileUrl, 0777, true );
+                }
+                move_uploaded_file( $file['tmp_name'], $filePath );
+                $model          = new TaskAttachment();
+                $model->name    = $file['name'];
+                $model->task_id = $_REQUEST['task_id'];
+                $model->path    = $fileName;
+                $response       = Yii::$app->getResponse();
+                if ($model->save()) {
+                    $response->setStatusCode( 200 );
+                }
+                $model->path       = Yii::$app->params['domainName'] . "/uploads/" . $model->task_id . "/" . $fileName;
+                $response->content = json_encode( $model->attributes );
+                $response->send();
+                Yii::$app->end();
+
+            }
+
+        }
+    }
