369e3c86c465b431143993ab498d298ceedd9a68
Anton Demydov <demydov.anton@gmail.com>
2015-02-06 15:15:07 +0200

    controllers : resolution and ticketnotes

---
+++ web/advanced/frontend/controllers/TicketnotesController.php
@@ -0,0 +1,45 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: godson
+     * Date: 15.10.14
+     * Time: 14:45
+     */
+
+    namespace frontend\controllers;
+
+    use common\models\SVServiceTicket;
+    use common\models\SVServiceTicketNotes;
+    use Yii;
+    use yii\rest\ActiveController;
+    use yii\filters\auth\QueryParamAuth;
+    use yii\helpers\ArrayHelper;
+
+
+    class TicketnotesController extends ActiveController
+    {
+        public $modelClass = 'common\models\SVServiceTicketNotes';
+
+        public function actionList()
+        {
+            return SVServiceTicket::getList( Yii::$app->user->getIdentity()->technition_id );
+        }
+
+        public function actionFind($id)
+        {
+            return SVServiceTicket::getSingleInfo( $id );
+        }
+
+        public function behaviors()
+        {
+            $behaviors = parent::behaviors();
+            $behaviors['authenticator'] = [
+                'class' => QueryParamAuth::className(),
+            ];
+            return $behaviors;
+        }
+
+        public function actionGetdispatch($task_id){
+            return SVServiceTicketNotes::find()->where(['Service_Ticket_Id'=>$task_id, 'Service_Tech_Id'=>Yii::$app->user->getIdentity()->technition_id])->orderBy(['Dispatch_Id'=>SORT_DESC])->one();
+        }
+    }
