12c03ff851d7d6c2ff50fb978f52f7a17beaa9fa
Mihail Shumilov <mschumilow@gmail.com>
2015-03-18 19:50:53 +0200

    added autocompliter

--- web/advanced/backend/views/user/_form.php
+++ web/advanced/backend/views/user/_form.php
@@ -13,22 +13,33 @@
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?php echo $form->errorSummary($model); ?>
+    <?php echo $form->errorSummary( $model ); ?>
 
     <?= $form->field( $model, 'username' )->textInput( [ 'maxlength' => 255 ] ) ?>
 
     <?= $form->field( $model, 'email' )->textInput( [ 'maxlength' => 255 ] ) ?>
-    
-    <?= $form->field($model, 'role')->dropDownList($model::$roleLabels); ?>
 
-    <?= $form->field( $model, 'technition_id' )->dropDownList(
-        ArrayHelper::map( \common\models\SVServiceTech::find()->with('employee')->asArray()->all(), 'Service_Tech_Id', function($element){
-;
-            return $element['Service_Tech_Id']." - ".$element['employee']['First_Name'].' '.$element['employee']['Last_Name'];
-        } ),['value'=>$model->technition_id]
-    )->label( "Technician" ) ?>
+    <?= $form->field( $model, 'role' )->dropDownList( $model::$roleLabels ); ?>
 
-    <?= $form->field( $model, 'password_hash' )->textInput( [ 'maxlength' => 255, 'value'=>""] )->label( "Password" ) ?>
+    <?= $form->field( $model, 'technition_id' )->widget( \kartik\select2\Select2::classname(), [
+        'data'          =>
+            ArrayHelper::map( \common\models\SVServiceTech::find()->with( 'employee' )->asArray()->all(),
+                'Service_Tech_Id', function ( $element ) {
+                    return $element['Service_Tech_Id'] . " - " . $element['employee']['First_Name'] . ' ' . $element['employee']['Last_Name'];
+                } ),
+        'language'      => 'en',
+        'value'         => $model->technition_id,
+        'options'       => [ 'Placeholder' => 'Select or type technician' ],
+        'pluginOptions' => [
+            'allowClear' => true,
+        ],
+    ] )->label( 'Technician' );
+    ?>
+
+    <?= $form->field( $model, 'password_hash' )->textInput( [
+        'maxlength' => 255,
+        'value'     => ""
+    ] )->label( "Password" ) ?>
 
     <div class="form-group">
         <?= Html::submitButton( $model->isNewRecord ? 'Create' : 'Update',
