2c85c37672093e3b160eb19b0fd11653979e5374
Mihail Shumilov <mschumilow@gmail.com>
2015-04-01 21:53:10 +0300

    added clear

--- web/advanced/console/controllers/SSLockController.php
+++ web/advanced/console/controllers/SSLockController.php
@@ -14,9 +14,7 @@ class SSLockController extends Controller {
      */
     public function actionIndex() {return;}
     public function actionClear() {
-        $res = SSLockTable::find("LockedTime > (CURRENT_TIMESTAMP - 300) and Form = 'mobile'");
-        var_dump($res);die;
-        echo "cron service runnning";
+        SSLockTable::deleteAll( "LockedTime > (CURRENT_TIMESTAMP - 300) and Form = 'mobile'" );
     }
 
 }
