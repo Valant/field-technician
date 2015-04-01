861e0d024f79112c6e9aba54c93c87ceebc73831
Mihail Shumilov <mschumilow@gmail.com>
2015-04-01 22:19:43 +0300

    fix ta

--- web/advanced/console/controllers/SSLockController.php
+++ web/advanced/console/controllers/SSLockController.php
@@ -9,12 +9,11 @@ use yii\console\Controller;
  * SSLockTable clear
  */
 class SSLockController extends Controller {
-    /**
-     *
-     */
-    public function actionIndex() {return;}
-    public function actionClear() {
+
+    public function actionIndex()
+    {
         SSLockTable::deleteAll( "LockedTime < DATEADD(n, -5, GETDATE()) and Form = 'mobile'" );
     }
+
 
 }
