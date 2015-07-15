fd6d5d8c2e3c28f1b5c9ae569a22a6d7f8806b34
Mihail Shumilov <mschumilow@gmail.com>
2015-07-15 11:13:51 +0300

    fix ta

--- web/advanced/frontend/controllers/Vendor/SearchAction.php
+++ web/advanced/frontend/controllers/Vendor/SearchAction.php
@@ -58,7 +58,7 @@ class SearchAction extends  Action  {
         $query = $modelClass::find();
         if(isset($params['VENDNM'])){
             $query->where("Vendor_Code LIKE :vendnm AND inactive = :inact");
-            $query->addParams([':vendnm'=>$params['Vendor_Code']."%"]);
+            $query->addParams([':vendnm'=>$params['VENDNM']."%"]);
             $query->addParams([':inact'=>'N']);
             unset($params['Vendor_Code']);
         }
