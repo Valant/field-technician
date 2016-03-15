148209452579cfde082a7778b0177ea8c3a4613c
Mihail Shumilov <mschumilow@gmail.com>
2016-03-15 15:33:49 +0200

    debug

--- web/advanced/common/models/SVServiceTicket.php
+++ web/advanced/common/models/SVServiceTicket.php
@@ -292,26 +292,30 @@
         public static function getList( $service_tech_id, $ticketStatus = [ 'SC', 'IP' ] )
         {
 
-            $dispatchList = SVServiceTicketDispatch::find()->select( 'SV_Service_Ticket_Dispatch.Dispatch_Id, SV_Service_Ticket.Service_Ticket_Id, SV_Service_Ticket.Scheduled_For, SV_Service_Ticket.Ticket_Number, SV_Problem.Description AS ProblemDescription,
+            $dispatchList = SVServiceTicketDispatch::find()->select( '
+                    SV_Service_Ticket_Dispatch.Dispatch_Id, SV_Service_Ticket.Service_Ticket_Id, SV_Service_Ticket.Scheduled_For, SV_Service_Ticket.Ticket_Number, SV_Problem.Description AS ProblemDescription,
                     AR_Customer.Customer_Name, ar_customer_site.ge1_description as  City,
                     SV_Service_Ticket.Ticket_Status,
-                    SS_LockTable.LockTable_Id,SS_LockTable.LockedByUser,SS_LockTable.LockedTime,SS_LockTable.Form' )
+                    SS_LockTable.LockTable_Id,SS_LockTable.LockedByUser,SS_LockTable.LockedTime,SS_LockTable.Form
+                    ' )
+                                                   ->from( 'SV_Service_Ticket' )
                                                    ->innerJoin( 'SV_Service_Ticket',
                                                        'SV_Service_Ticket.Service_Ticket_Id = SV_Service_Ticket_Dispatch.Service_Ticket_Id' )
-                                                    ->innerJoin( 'SV_Problem', 'SV_Problem.Problem_Id = SV_Service_Ticket.Problem_Id' )
-                                                    ->innerJoin( 'AR_Customer', 'AR_Customer.Customer_Id = SV_Service_Ticket.Customer_Id' )
-                                                    ->innerJoin( 'AR_Customer_Site',
-                                                        'AR_Customer_Site.Customer_Site_Id = SV_Service_Ticket.Customer_Site_Id' )
-                                                    ->leftJoin( 'SS_LockTable',
-                                                        'SV_Service_Ticket.Ticket_Number = SS_LockTable.Code and SS_LockTable.Table_Name = "sv_service_ticket"' )
+                                                   ->innerJoin( 'SV_Problem',
+                                                       'SV_Problem.Problem_Id = SV_Service_Ticket.Problem_Id' )
+                                                   ->innerJoin( 'AR_Customer',
+                                                       'AR_Customer.Customer_Id = SV_Service_Ticket.Customer_Id' )
+                                                   ->innerJoin( 'AR_Customer_Site',
+                                                       'AR_Customer_Site.Customer_Site_Id = SV_Service_Ticket.Customer_Site_Id' )
+                                                   ->leftJoin( 'SS_LockTable',
+                                                       'SV_Service_Ticket.Ticket_Number = SS_LockTable.Code and SS_LockTable.Table_Name = "sv_service_ticket"' )
                                                    ->where( [
                                                        "SV_Service_Ticket.Ticket_Status"            => $ticketStatus,
                                                        "SV_Service_Ticket_Dispatch.Service_Tech_Id" => $service_tech_id,
                                                        "SV_Service_Ticket_Dispatch.Resolution_Id"   => 1
                                                    ] )
 //                                                   ->groupBy( 'SV_Service_Ticket_Dispatch.Dispatch_Id' )
-                                                   ->orderBy( 'SV_Service_Ticket.Scheduled_For',
-                    SORT_DESC )->all();
+                                                   ->orderBy( 'SV_Service_Ticket.Scheduled_For', SORT_DESC )->all();
             return $dispatchList;
             $ticketIds = [ ];
             foreach ($dispatchList as $dp) {
