8f26150de9c65ca07f3b83992346a4570e6564d3
Mihail Shumilov <mschumilow@gmail.com>
2016-04-13 15:42:16 +0300

    fix save without sign

--- web/advanced/frontend/controllers/UserController.php
+++ web/advanced/frontend/controllers/UserController.php
@@ -51,39 +51,35 @@
                 throw new HttpException( '503', 'No email' );
             }
             if (filter_var( $postData['email'], FILTER_VALIDATE_EMAIL )) {
-                $img      = $postData['sign'];
-                $img      = str_replace( 'data:image/png;base64,', '', $img );
-                $img      = str_replace( ' ', '+', $img );
-                $fileData = base64_decode( $img );
+                if(isset($postData['sign'])) {
+                    $img      = $postData['sign'];
+                    $img      = str_replace('data:image/png;base64,', '', $img);
+                    $img      = str_replace(' ', '+', $img);
+                    $fileData = base64_decode($img);
 
-                $fileName = mt_rand( 0, PHP_INT_MAX ) . "_userSign";
-                $fileUrl  = "/web/uploads/" . $postData['task_id'] . "/";
-                $filePath = Yii::getAlias( Yii::$app->params['filePath'] ) . $fileUrl . $fileName;
-                if ( ! is_dir( Yii::getAlias( Yii::$app->params['filePath'] ) . $fileUrl )) {
-                    mkdir( Yii::getAlias( Yii::$app->params['filePath'] ) . $fileUrl, 0777, true );
+                    $fileName = mt_rand(0, PHP_INT_MAX) . "_userSign";
+                    $fileUrl  = "/web/uploads/" . $postData['task_id'] . "/";
+                    $filePath = Yii::getAlias(Yii::$app->params['filePath']) . $fileUrl . $fileName;
+                    if ( ! is_dir(Yii::getAlias(Yii::$app->params['filePath']) . $fileUrl)) {
+                        mkdir(Yii::getAlias(Yii::$app->params['filePath']) . $fileUrl, 0777, true);
+                    }
+                    file_put_contents($filePath, $fileData);
+
+                    $model          = new TaskAttachment();
+                    $model->name    = 'User sign';
+                    $model->task_id = $postData['task_id'];
+                    $model->path    = $fileName;
+                    $model->save();
+
+
+                    $signUrl = Yii::$app->params['domainName'] . "/uploads/" . $model->task_id . "/" . $fileName;
                 }
-                file_put_contents( $filePath, $fileData );
 
-                $model          = new TaskAttachment();
-                $model->name    = 'User sign';
-                $model->task_id = $postData['task_id'];
-                $model->path    = $fileName;
-                $model->save();
-
-
-                $signUrl = Yii::$app->params['domainName'] . "/uploads/" . $model->task_id . "/" . $fileName;
-
-//                Yii::$app->mail->compose( [ 'html' => '@frontend/views/mail-templates/receipt' ], [
-//                    'timing'  => $postData['time'],
-//                    'parts'   => $postData['parts'],
-//                    'signUrl' => $signUrl
-//                ] )
-//                               ->setFrom( 'no-reply@afap.com' )
-//                               ->setTo( $postData['email'] )
-//                               ->setSubject( 'Receipt' )
-//                               ->send();
-
-                return [ 'file' => $signUrl, 'sign' => $signUrl ];
+                if($signUrl) {
+                    return ['file' => $signUrl, 'sign' => $signUrl];
+                }else{
+                    return ['status'=>true];
+                }
             } else {
                 throw new HttpException( '503', 'Not valid email' );
             }
