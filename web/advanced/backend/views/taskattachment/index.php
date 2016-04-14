f388fc24c0d33ff48b1b545463de6537975a74a9
Mihail Shumilov <mschumilow@gmail.com>
2016-04-14 15:01:19 +0300

    switch to

--- web/advanced/backend/views/taskattachment/index.php
+++ web/advanced/backend/views/taskattachment/index.php
@@ -37,6 +37,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 },
             ],
             'name',
+            'sign_name',
 
             ['class' => 'yii\grid\ActionColumn'],
         ],
--- web/advanced/backend/views/taskattachment/view.php
+++ web/advanced/backend/views/taskattachment/view.php
@@ -32,6 +32,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'task_id',
             'path',
             'name',
+            'sign_name'
         ],
     ]) ?>
 
