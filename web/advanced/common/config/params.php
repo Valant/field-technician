1d4b61e63c069b4d703adb817456649ebdedca68
Mihail Shumilov <mschumilow@gmail.com>
2016-07-14 09:31:34 +0300

    add production URL

--- web/advanced/common/config/params.php
+++ web/advanced/common/config/params.php
@@ -4,5 +4,5 @@
         'supportEmail'                  => 'support@example.com',
         'user.passwordResetTokenExpire' => 3600,
         'api.url'                       => 'http://sedonaapi.afap.com',
-        'sign.api.url'                  => 'http://swosvctest.afap.com'
+        'sign.api.url'                  => 'http://swosvc.afap.com'
     ];
