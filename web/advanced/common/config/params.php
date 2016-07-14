1c0ccff85a3e46d9c8243eb4b4eec1436e2278b3
Mihail Shumilov <mschumilow@gmail.com>
2016-07-14 09:35:29 +0300

    revert changes

--- web/advanced/common/config/params.php
+++ web/advanced/common/config/params.php
@@ -3,6 +3,6 @@
         'adminEmail'                    => 'admin@example.com',
         'supportEmail'                  => 'support@example.com',
         'user.passwordResetTokenExpire' => 3600,
-        'api.url'                       => 'http://swosvc.afap.com',
+        'api.url'                       => 'http://sedonaapi.afap.com',
         'sign.api.url'                  => 'http://swosvc.afap.com'
     ];
